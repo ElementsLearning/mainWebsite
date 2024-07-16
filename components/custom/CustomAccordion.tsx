c8df68474aea10d2e30f9f609e37b3952eeb3e42
Taha Shah <tahashah411@gmail.com>
2024-07-16 17:25:45 +0500

    fix disabled rendering

--- components/custom/CustomAccordion.tsx
+++ components/custom/CustomAccordion.tsx
@@ -22,14 +22,14 @@ export const CustomAccordion: React.FC<CustomAccordionProps> = ({containerClass,
     <Accordion onValueChange={(value: string | string[]) => setOpened(value)} className={containerClass} type={type} collapsible>
       {items.map(item => 
         <AccordionItem key={item.id} value={item.id}>
-          <AccordionTrigger className={`p-1 rounded-full hover:no-underline rounded-br-none font-bold text-xs xs:text-base sm:text-xl md:text-2xl lg:text-xl flex items-center gap-2 group transition-colors ${item.id !== opened && changeHeaders ? "bg-transparent delay-0 duration-0" : item.disabled ? "bg-gray-300 text-gray-600" : "bg-[#FBBA41] duration-200"}`}>
+          <AccordionTrigger className={`p-1 rounded-full hover:no-underline rounded-br-none font-bold text-xs xs:text-base sm:text-xl md:text-2xl lg:text-xl flex items-center gap-2 group transition-colors ${item.id !== opened && changeHeaders ? "bg-transparent delay-0 duration-0" : item.disabled === true ? "bg-gray-300 text-gray-600" : "bg-[#FBBA41] duration-200"}`}>
             <div className="size-6 xs:size-8 sm:size-12 xl:size-16 relative">
               {!(item.id === opened || (opened instanceof Array && opened.filter(id => item.id === id).length !== 0)) ? 
               <>
               {changeHeaders ? 
               <>
               <div className="absolute inset-0 transition-opacity duration-200 size-full opacity-100 group-hover:opacity-0">
-                <HexagonPlayButton outerColor={item.disabled === false ? "#aaa" : "#FCBA42"} innerColor={"#FFFFFF"} />
+                <HexagonPlayButton outerColor={item.disabled === true ? "#aaa" : "#FCBA42"} innerColor={"#FFFFFF"} />
               </div>
               <div className="absolute inset-0 transition-opacity duration-500 size-full opacity-0 group-hover:opacity-100">
                 <HexagonPlayButton outerColor="#FFFFFF" innerColor="#000000" />
