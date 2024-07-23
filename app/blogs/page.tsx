ffb28086dc805358dd4b02aea593836057b58eb8
Taha Shah <tahashah411@gmail.com>
2024-07-23 09:52:03 +0500

    add search bar

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -2,9 +2,13 @@
 
 import { StaggeredHeaders } from "@/components/custom/StaggeredHeaders";
 import { Footer } from "@/components/pages/Footer/Footer";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { allBlogs } from "@/constants/Blogs/allBlogs";
 import { Blog } from "@/constants/Blogs/blog";
+import { SearchIcon, XIcon } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const BlogCard: React.FC<Blog> = ({id, title, author, date, summary}) => {
   return (
@@ -23,6 +27,9 @@ const BlogCard: React.FC<Blog> = ({id, title, author, date, summary}) => {
 
 export default function AllBlogs() {
 
+  const [searchTerm, setSearchTerm] = useState("")
+  const [filteredBlogs, setFilteredBlogs] = useState(allBlogs)
+
   return (
     <div className="flex flex-col">
       <section className="bg-[#F3EEE8] p-2 xs:p-8 xs:px-10 md:px-20 w-full flex flex-col-reverse min-[400px]:flex-row  min-[400px]:gap-4 lg:gap-20 justify-center items-center">
@@ -31,12 +38,32 @@ export default function AllBlogs() {
           <img src="/newsandupdatespage/newsandupdates.png" alt="" />
         </div>
       </section>
-      <div className="p-4 md:p-8 xl:p-16 flex flex-wrap gap-4">
-        {allBlogs.map(blog => <BlogCard key={blog.id} {...blog} />)}
-        {allBlogs.map(blog => <BlogCard key={blog.id} {...blog} />)}
-        {allBlogs.map(blog => <BlogCard key={blog.id} {...blog} />)}
-        {allBlogs.map(blog => <BlogCard key={blog.id} {...blog} />)}
-      </div>
+      <section className="flex flex-col gap-8 p-4 md:p-8 xl:p-16 items-center">
+        <div className="flex gap-2 w-full justify-center">
+          <Input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search..." className="w-full max-w-sm" />
+          <Button onClick={() => {
+            setFilteredBlogs(allBlogs.filter(blog => blog.title.toLowerCase().includes(searchTerm.toLowerCase())))
+            setSearchTerm("")
+          }} variant="outline" size={"icon"}>
+            <SearchIcon className="size-4" />
+          </Button>
+          {filteredBlogs.length !== allBlogs.length && 
+          <Button onClick={() => {
+            setFilteredBlogs(allBlogs)
+            setSearchTerm("")
+          }} variant={"secondary"} size={"icon"}>
+            <XIcon className="size-4" />
+          </Button>}
+        </div>
+        <div className="flex flex-wrap gap-4">
+          {filteredBlogs.map(blog => <BlogCard key={blog.id} {...blog} />)}
+          {/* 
+          {filteredBlogs.map(blog => <BlogCard key={blog.id} {...blog} />)}
+          {filteredBlogs.map(blog => <BlogCard key={blog.id} {...blog} />)}
+          {filteredBlogs.map(blog => <BlogCard key={blog.id} {...blog} />)}
+           */}
+        </div>
+      </section>
       <Footer />
     </div>
   )
