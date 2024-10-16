7e5ead8d744deccec877081b280a8d6b3c4fa667
Umair Muhammad <umairijaz2772@gmail.com>
2024-10-16 10:35:50 +0500

    adjust team

--- constants/team.ts
+++ constants/team.ts
@@ -26,66 +26,6 @@ export const team: TeamMember[] = [
     corner: "br"
   },
   {
-    name: "Janita Khan",
-    designation: "Program Manager & Math Team Lead",
-    corner: "bl"
-  },
-  {
-    name: "Munazzah Younas",
-    designation: "Business Development & Marketing Manager",
-    corner: "tr"
-  },
-  {
-    name: "Mahjabeen Butt",
-    designation: "Math Lead Trainer & Researcher",
-    corner: "br"
-  },
-  {
-    name: "Saad Ali",
-    designation: "Admin Assistant",
-    corner: "br"
-  },
-  {
-    name: "Aneeqa Sehar",
-    designation: "IT Manager",
-    corner: "br"
-  },
-  {
-    name: "Fizza Haq",
-    designation: "Quality Assurance Lead",
-    corner: "br"
-  },
-  {
-    name: "Hamza Mehmood",
-    designation: "Knowledge Officer",
-    corner: "br"
-  },
-  {
-    name: "Waqar Ahmed",
-    designation: "Assistant Manager, KM & QA ",
-    corner: "br"
-  },
-  {
-    name: "Memoona Ahmad",
-    designation: "Maths Researcher",
-    corner: "br"
-  },
-  {
-    name: "Sadia Maqbool",
-    designation: "Science Researcher",
-    corner: "br"
-  },
-  {
-    name: "Sana Naeem",
-    designation: "Maths Trainer & Researcher",
-    corner: "br",
-  },
-  {
-    name: "Naveera Khan",
-    designation: "Science Researcher",
-    corner: "br"
-  },
-  {
     name: "Hira Abbasi",
     designation: "Dir HR & OD",
     corner: "br"
@@ -96,18 +36,93 @@ export const team: TeamMember[] = [
     corner: "br"
   },
   {
+    name: "Hamza Mehmood",
+    designation: "Knowledge Officer",
+    corner: "br"
+  },
+  {
+    name: "Waqar Ahmed",
+    designation: "Asst.Project Manager (Lead QA, NOC/Copyright) & KM ",
+    corner: "br"
+  },
+  {
+    name: "Fizza Haq",
+    designation: "Quality Assurance Assistant",
+    corner: "br"
+  },
+  {
+    name: "Janita Khan",
+    designation: "Program Manager & Math Team Lead",
+    corner: "bl"
+  },{
+    name: "Munazzah Younas",
+    designation: "Business Development & Marketing Manager",
+    corner: "tr"
+  },
+  {
     name: "Muhammad Abdullah",
     designation: "Communications Manager",
     corner: "br"
+  },
+  {
+    name: "Aneeqa Sehar",
+    designation: "IT Manager",
+    corner: "br"
+  },
+  {
+    name: "Memoona Ahmad",
+    designation: "Maths Researcher",
+    corner: "br"
+  },
+  {
+    name: "Mahjabeen Butt",
+    designation: "Math Lead Trainer & Researcher",
+    corner: "br"
+  },
+  {
+    name: "Sana Naeem",
+    designation: "Maths Trainer & Researcher",
+    corner: "br",
   },
   {
     name: "Zoha Rizvi",
     designation: "Math Trainer & Researcher",
     corner: "br"
   },
+  
+  {
+    name: "Adan Haider",
+    designation: "Math Research (Intern)",
+    corner: "br"
+  },
+  {
+    name: "Sadia Maqbool",
+    designation: "Science Researcher",
+    corner: "br"
+  },
+  {
+    name: "Naveera Khan",
+    designation: "Science Researcher",
+    corner: "br"
+  },
   {
     name: "Yasir Anwar",
     designation: "Admin Manager",
+    corner: "br"
+  },
+  {
+    name: "Saad Ali",
+    designation: "Admin Assistant",
+    corner: "br"
+  },
+  {
+    name: "Taha Shah",
+    designation: "Web Developer",
+    corner: "br"
+  },
+  {
+    name: "Umair Ijaz",
+    designation: "Web Developer (Intern)",
     corner: "br"
   },
   {
@@ -118,21 +133,6 @@ export const team: TeamMember[] = [
   {
     name: "Amna Rafique",
     designation: "Graphics Illustrator",
-    corner: "br"
-  },
-  {
-    name: "Taha Shah",
-    designation: "Web Developer",
-    corner: "br"
-  },
-  {
-    name: "Umair Ijaz",
-    designation: "Web Developer Intern",
-    corner: "br"
-  },
-  {
-    name: "Adan Haider",
-    designation: "Math Research Intern",
     corner: "br"
   },
   
