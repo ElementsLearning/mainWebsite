f00543f243dee3c55cf1b04429a7d02f92681e27
Umair Muhammad <umairijaz2772@gmail.com>
2024-10-29 10:01:55 +0500

    adjust aspect ratio

--- app/about/TeamCard.tsx
+++ app/about/TeamCard.tsx
@@ -12,7 +12,7 @@ export const TeamCard: React.FC<TeamMember> = ({name, designation, corner}) => {
   const cornerClass = `rounded-${corner}-none sm:rounded-${corner}-none md:rounded-${corner}-none `
 
   return (
-    <div className={`relative group overflow-hidden rounded-[25px] sm:rounded-[40px] md:rounded-[50px] border border-black flex-1 aspect-[3/2] ${cornerClass} bg-orange-50`}>
+    <div className={`relative group overflow-hidden rounded-[25px] sm:rounded-[40px] md:rounded-[50px] border border-black flex-1 aspect-[1.5] ${cornerClass} bg-orange-50`} > 
       <img src={`/team/${name}.jpg`} alt="Missing" className="size-full grayscale group-hover:grayscale-0" />
       <div className="absolute inset-0 translate-y-full overflow-hidden group-hover:translate-y-0 gap-1 transition-transform duration-500 flex flex-col justify-end p-3 sm:p-4 md:p-5 lg:p-4 xl:p-6 bg-black/40">
         <p className="text-sm sm:text-base md:text-lg lg:text-base xl:text-xl font-bold text-[#FBBA42] leading-none">{name}</p>
