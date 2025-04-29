b8570d7dc01ed6a45a8b62c2de2ce6b268af0fea
TahaShah141 <tahashah411@gmail.com>
2025-04-29 16:00:01 +0500

    add safety to orders route

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -180,6 +180,7 @@ export default function Orders() {
   })
 
   return (
+    <Authenticator>
     <div className="flex flex-col h-screen gap-2 p-4">
       <div className="flex flex-wrap gap-2 justify-center items-center">
         <div className="flex gap-2 items-center">
@@ -206,5 +207,6 @@ export default function Orders() {
       </div>
       <OrdersTable orders={orders} loading={loading} toggleStatus={toggleStatus} filters={filters} />
     </div>
+    </Authenticator>
   )
 }
