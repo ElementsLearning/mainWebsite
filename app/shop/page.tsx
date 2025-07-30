e0ed0989c4cad8677c9b8b701812e4bf8d9a035f
TahaShah141 <tahashah411@gmail.com>
2025-07-30 16:21:27 +0500

    improve shop form

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,18 +1,16 @@
 "use client"
 
-import { CustomerType, OrderType } from "@/constants/Shop/shop";
+import { OrderType } from "@/constants/Shop/shop";
 import { DELIVERY_RATE, TAX_RATE, shopItems } from "@/constants/Shop/shopItems";
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
-import { Image, uploadImage } from "@/lib/utils";
-import { defaultCustomer, defaultFilledCustomer } from "@/constants/Shop/defaultCustomer";
+import { uploadImage } from "@/lib/utils";
+import { defaultCustomer } from "@/constants/Shop/defaultCustomer";
 
-import { FormInput } from "./FormInput";
-import { Input } from "@/components/ui/input";
 import { ItemCard } from "@/components/shopComponents/ItemCard";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
+import { ShopForm } from "@/components/shopComponents/ShopForm";
+import { ShopFormType } from "@/constants/Shop/shopSchema";
 
 export default function Shop() {
 
@@ -21,13 +19,7 @@ export default function Shop() {
     return acc;
   }, {} as { [key: string]: number }));
   const [state, setState] = useState("");
-  const [customerInfo, setCustomerInfo] = useState<CustomerType>(defaultCustomer)
-  const [orderNotes, setOrderNotes] = useState<string>("")
-  const [paymentImage, setPaymentImage] = useState<Image>(null)
   const [loading, setLoading] = useState(false)
-  const [infoOpen, setInfoOpen] = useState(false)
-  const [termsOpen, setTermsOpen] = useState(false)
-  const [conditionsChecked, setConditionsChecked] = useState(false)
   const { toast } = useToast()
 
   const updateQuantity = (itemName: string, quantity: number) => {
@@ -42,17 +34,17 @@ export default function Shop() {
     return total + itemTotal;
   }, 0);
 
-  const submitOrder = async () => {
+  const submitOrder = async (data: ShopFormType, paymentImage: File) => {
     setLoading(true)
     const link = await uploadImage(paymentImage)
     const order: OrderType = {
-      customerInfo,
+      customerInfo: data,
       items: shopItems.map((item) => ({
         name: item.name,
         quantity: cart[item.name],
       })).filter(i => i.quantity > 0),
       paymentURL: link,
-      notes: orderNotes,
+      notes: data.orderNotes || "",
       completed: false
     }
     const res = await fetch("/api/orders/new", {
@@ -62,8 +54,8 @@ export default function Shop() {
       },
       body: JSON.stringify({ order }),
     })
-    const data = await res.json()
-    console.log(data)
+    const resData = await res.json()
+    console.log(resData)
     if (res.status === 200) {
       toast({
         title: "Order Placed",
@@ -74,23 +66,10 @@ export default function Shop() {
         acc[item.name] = 0;
         return acc;
       }, {} as { [key: string]: number }));
-      // await fetch("/api/mail/order", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({ order, totalAmount }),
-      // })
-      setCustomerInfo(defaultCustomer)
-      setPaymentImage(null)
-      setOrderNotes("")
       setState("")
     }
+    setLoading(false)
   }
-
-  const { name, address, email, phone, city, postalCode } = customerInfo
-  const notFilled = name === "" || address === "" || email === "" || phone === "" || city === "" || postalCode === "" || paymentImage === null
-  const invalidForm = notFilled || phone.length !== 11 || email.length < 5 || email.indexOf("@") === -1 || email.indexOf(".") === -1
 
   return (
     <div className="flex relative h-screen flex-col items-center">
@@ -155,109 +134,11 @@ export default function Shop() {
       {state === "FORM" && (
         <div className="h-full fixed inset-0 bg-black/80 z-50 flex justify-center items-center p-4 sm:p-8" onClick={() => setState("")}>
           <div onClick={(e) => e.stopPropagation()} className="w-full sm:w-4/5 lg:w-2/3 xl:w-1/2 bg-white rounded-[20px] sm:rounded-br-none rounded-br-none sm:rounded-[50px] flex flex-col p-6 gap-2 lg:p-12">
-            <p onClick={() => setCustomerInfo(defaultFilledCustomer)} className="font-bold text-[#FBBA41] text-xl sm:text-2xl md:text-3xl lg:text-4xl">Checkout Form</p>
-            <div className="flex flex-col gap-4">
-              <FormInput label="Full Name" value={name} setValue={(s) => setCustomerInfo({...customerInfo, name: s})} />
-              <div className="flex flex-col sm:flex-row gap-2">
-                <FormInput label="Email" value={email} setValue={(s) => setCustomerInfo({...customerInfo, email: s})} />
-                <FormInput label="Phone Number" value={phone} setValue={(s) => setCustomerInfo({...customerInfo, phone: s})} />
-              </div>
-              <div className="flex flex-col sm:flex-row gap-2">
-                <FormInput label="City" value={city} setValue={(s) => setCustomerInfo({...customerInfo, city: s})} />
-                <FormInput label="Post Code" value={postalCode} setValue={(s) => setCustomerInfo({...customerInfo, postalCode: s})} />
-              </div>
-              <FormInput label="Shipping Address" value={address} setValue={(s) => setCustomerInfo({...customerInfo, address: s})} />
-              <div className="flex flex-col justify-between gap-2">
-                <div className="flex justify-between items-center">
-                  <p className="uppercase tracking-widest text-black">Payment Proof</p>
-                  <HoverCard open={infoOpen} onOpenChange={setInfoOpen}>
-                    <HoverCardTrigger onClick={() => setInfoOpen(!infoOpen)} className="cursor-pointer">
-                      <svg className="size-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="12" cy="12" r="10"/><path d="M12 16v-4m0-4h.01"/></g></svg>
-                    </HoverCardTrigger>
-                    <HoverCardContent className="w-80 flex justify-center">
-                      <div className="flex flex-col gap-2 text-sm">
-                        <p className="text-lg font-bold">Payment Instructions</p>
-                        <p className="text-sm">Please make the payment to the following account:</p>
-                        <div className="flex justify-between items-center">
-                          <p className="font-bold">Total Amount</p>
-                          <p>{totalAmount}</p>
-                        </div>
-                        <div className="flex justify-between items-center">
-                          <p className="font-bold">Account Name</p>
-                          <p>Elements Learning (Pvt) Limited</p>
-                        </div>
-                        <div className="flex justify-between items-center">
-                          <p className="font-bold">Account IBAN</p>
-                          <p>PK53ALFH5627005001269166</p>
-                        </div>
-                        <div className="flex justify-between items-center">
-                          <p className="font-bold">Bank</p>
-                          <p>Bank Alfalah Islamic</p>
-                        </div>
-                        <div className="flex justify-between items-center">
-                          <p className="font-bold">Branch Code</p>
-                          <p>5627</p>
-                        </div>
-                      </div>
-                    </HoverCardContent>
-                  </HoverCard>
-                </div>
-                <Input className="" type="file" accept="image/*" onChange={(e) => setPaymentImage(e.target.files?.[0] || null)} />
-              </div>
-              <div className="flex flex-col gap-2">
-                <p className="uppercase tracking-widest text-black">Additional Notes</p>
-                <Textarea rows={3} placeholder="Message Here (optional)" value={orderNotes} onChange={(e) => setOrderNotes(e.target.value)} className="border p-2 rounded-md text-sm" />
-              </div>
-              <div className="flex gap-2 items-center">
-                <Input className="size-4" type="checkbox" id="terms" checked={conditionsChecked} onChange={(e) => setConditionsChecked(e.target.checked)} />
-                <label htmlFor="terms" className="text-sm sm:text-base flex-1 flex flex-wrap gap-2">
-                  <HoverCard open={termsOpen} onOpenChange={setTermsOpen}>
-                    <p className="">
-                      I agree to the
-                      <span className="mr-2"><HoverCardTrigger onClick={() => setTermsOpen(!termsOpen)} className="text-[#FBBA41] font-bold cursor-pointer"> Terms and Conditions</HoverCardTrigger></span>
-                      and confirm payment is complete.
-                    </p>
-                    <HoverCardContent className="flex flex-col w-96">
-                      <TermsAndConditions />
-                    </HoverCardContent>
-                  </HoverCard>
-                </label>
-              </div>
-              <button disabled={loading || invalidForm || !conditionsChecked} onClick={submitOrder} type="submit" className="bg-[#404791] disabled:bg-gray-500 text-white font-bold text-2xl px-4 py-2 rounded-full hover:bg-[#6CC3E0] transition duration-300">
-                Submit
-              </button>
-            </div>
+            <p className="font-bold text-[#FBBA41] text-xl sm:text-2xl md:text-3xl lg:text-4xl">Checkout Form</p>
+            <ShopForm totalAmount={totalAmount} submitOrder={submitOrder} loading={loading} />
           </div>
         </div>
       )}
-    </div>
-  )
-}
-
-const TermsAndConditions = () => {
-  return (
-    <div className="flex flex-col gap-2 text-xs sm:text-sm">
-      <p className="font-bold">By placing an order on our website, you agree to the following terms:</p>
-      <ol className="list-decimal list-inside space-y-2">
-        <li>
-          <strong>Prepayment Required</strong> - We only accept prepaid orders. Payment must be made to the provided bank details before order processing.
-        </li>
-        <li>
-          <strong>Payment Confirmation</strong> - You must upload a valid payment receipt. Orders without a receipt will not be processed.
-        </li>
-        <li>
-          <strong>Processing Time</strong> - Orders are processed within 5-7 working days after payment confirmation.
-        </li>
-        <li>
-          <strong>No Refunds or Cancellations</strong> - Once an order is placed and payment is made, it cannot be canceled or refunded.
-        </li>
-        <li>
-          <strong>Shipping & Delivery</strong> - Delivery times may vary depending on location. You will be notified once your order is shipped.
-        </li>
-        <li>
-          <strong>Contact for Support</strong> - If you have any issues, please contact our support team for assistance.
-        </li>
-      </ol>
     </div>
   )
 }
